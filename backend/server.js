72ddcd3a86081e5499081bcd323e41f92699ef80
jen <jenhung.jocker@gmail.com>
2023-04-04 19:23:06 +0800

    add backend service

---
+++ backend/server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+require('dotenv').config();
+
+// Connect to MongoDB
+const uri = process.env.MONGODB_URI;
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Create feedback schema and model
+const feedbackSchema = new mongoose.Schema({
+  uid: String,
+  questionJSON: String,
+  feedback: String,
+  comment: String,
+});
+
+const Feedback = mongoose.model('Feedback', feedbackSchema);
+
+// Create an express app
+const app = express();
+app.use(express.json());
+
+// Setup swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// POST feedback endpoint
+app.post('/feedback', async (req, res) => {
+  try {
+    const feedback = new Feedback(req.body);
+    await feedback.save();
+    res.status(201).send(feedback);
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
