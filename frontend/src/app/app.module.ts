7ffdcb5c5a1609238fd70d1ca1c76ef93d58df02
jen <jenhung.jocker@gmail.com>
2023-04-17 00:39:08 +0800

    local test ok

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,18 +8,29 @@ import { FeedbackService } from './feedback.service';
 import { HttpClientModule } from '@angular/common/http';
 import { FeedbackComponent } from './feedback/feedback.component';
 import { FooterComponent } from './footer/footer.component';
+
+import { AuthService } from './auth.service';
+import { environment } from '../environments/environment';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { SignInComponent } from './signin/signin.component';
+import { initializeApp } from "firebase/app";
+
 @NgModule({
   declarations: [
     AppComponent,
     FeedbackComponent,
     FooterComponent,
+    SignInComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
   ],
-  providers: [FeedbackService],
+  providers: [FeedbackService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ frontend/src/app/auth.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { AuthProvider, GoogleAuthProvider } from 'firebase/auth';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(
+    public afAuth: AngularFireAuth // Inject Firebase auth service
+  ) {
+  }
+  // Sign in with Google
+  GoogleAuth() {
+    return this.AuthLogin(new GoogleAuthProvider());
+  }
+  // Auth logic to run auth providers
+  AuthLogin(provider: AuthProvider) {
+    return this.afAuth
+      .signInWithPopup(provider)
+      .then((result) => {
+        console.log('You have been successfully logged in!');
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+}
