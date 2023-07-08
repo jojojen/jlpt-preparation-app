df82c8fbe752625c00d406dc880855e7ae561149
jen <jenhung.jocker@gmail.com>
2023-07-08 14:54:22 +0800

    replace model with gpt-3.5-turbo

--- frontend/src/app/gpt-3.service.ts
+++ frontend/src/app/gpt-3.service.ts
@@ -20,13 +20,13 @@ export class Gpt3Service {
     try {
       console.log("prompt: " + this.selectedPrompt);
       const response = await axios.post(
-        'https://api.openai.com/v1/engines/text-davinci-003/completions',
+        'https://api.openai.com/v1/chat/completions',  // Adjust the API endpoint
         {
-          prompt: this.selectedPrompt,
-          max_tokens: 320,
-          n: 1,
-          stop: null,
-          temperature: 0.7,
+          model: 'gpt-3.5-turbo',
+          messages: [
+            {"role": "system", "content": "You are a professional japanese tutor AI proficient in JLPT questions and having extensive experience in teaching Japanese."},
+            {"role": "user", "content": this.selectedPrompt}
+          ],
         },
         {
           headers: {
@@ -38,17 +38,17 @@ export class Gpt3Service {
 
       if (response.data.choices && response.data.choices.length > 0) {
         const choice = response.data.choices[0];
-        console.log('GPT-3 response text:', choice.text);
-        const extractedJson = this.jsonExtractor.extractJson(choice.text);
+        console.log('GPT-3.5-turbo response text:', choice.message.content);
+        const extractedJson = this.jsonExtractor.extractJson(choice.message.content);
         if (extractedJson) {
           return { resultText: JSON.stringify(extractedJson), error: '' };
         } else {
-          console.error('Error fetching GPT-3 response: Unable to extract JSON');
+          console.error('Error fetching GPT-3.5-turbo response: Unable to extract JSON');
           return { resultText: '', error: 'Unable to extract JSON' };
         }
       }
     } catch (error) {
-      console.error('Error fetching GPT-3 response:', error);
+      console.error('Error fetching GPT-3.5-turbo response:', error);
     }
     return { resultText: '', error: '' };
   }
--- frontend/src/app/prompts.ts
+++ frontend/src/app/prompts.ts
@@ -1,5 +1,5 @@
 // prompts.ts
-const sharedPrompt = `As a professional japanese tutor AI proficient in JLPT questions and having extensive experience in teaching Japanese, please create ONE test question for understanding everyday Japanese conversation, following these conditions:
+const sharedPrompt = `Please create ONE test question for understanding everyday Japanese conversation, following these conditions:
 1. Do not use the words in the question directly in the options.
 2. Of the two choices, one should conform to common sense and grammar, while the other should clearly feel out of place when considering common sense or grammar.
 3. The correct answer must follow common sense and grammar.
